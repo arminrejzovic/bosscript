6298a01b417b2673ead3cd8a8db3b7430934a50b
Armin <rejzovicarmin@gmail.com>
2023-03-16 02:01:47 +0100

    Added performance tests

--- src/test/kotlin/benchmarks/PerformanceTests.kt
+++ src/test/kotlin/benchmarks/PerformanceTests.kt
@@ -1,4 +1,51 @@
 package benchmarks
 
+import org.junit.jupiter.api.Test
+import udemy.Array
+import udemy.Interpreter
+
 class PerformanceTests {
+    @Test
+    fun testFibonacciFunction(){
+        val src = """
+            funkcija fib(n: broj): broj{
+                ako (n <= 1){
+                    vrati n;
+                }
+                inace {
+                    vrati fib(n-1) + fib(n-2);
+                }
+            }
+            
+            var n = fib(10);
+            ispis(n);
+        """.trimIndent()
+
+        val interpreter = Interpreter()
+        val result = interpreter.evaluateProgram(src)
+    }
+
+    @Test
+    fun testFactorialFunction(){
+        val src = """
+            funkcija fakt(n: broj): broj{
+                ako (n == 0){
+                    vrati 1;
+                }
+                inace {
+                    vrati n * fakt(n-1);
+                }
+            }
+            
+            var n = fakt(169);
+            ispis(n);
+            ispis(6969);
+            ispis(1.2);
+            ispis(1.04);
+        """.trimIndent()
+
+        val interpreter = Interpreter()
+        val result = interpreter.evaluateProgram(src)
+        println(result.last())
+    }
 }
