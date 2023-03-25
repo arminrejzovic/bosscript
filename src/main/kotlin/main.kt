191eaf0a8c733d37a9d1ed483fd13a58d6b92062
Armin <rejzovicarmin@gmail.com>
2023-03-25 22:47:06 +0100

    Added support for passing real file as program argument

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+import interpreter.Interpreter
+import java.io.File
+
+fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        val filename = args[0]
+        val file = File(filename)
+
+        if (file.exists()) {
+            val interpreter = Interpreter()
+            interpreter.evaluateProgram(file.readText())
+        } else {
+            throw Exception("No such file found: $filename")
+        }
+    } else {
+        throw Exception("No file to evaluate")
+    }
+}
