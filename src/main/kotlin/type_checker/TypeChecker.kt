85627f9b32593bd1e3a353700747cbaff1284ba5
Armin <rejzovicarmin@gmail.com>
2023-04-12 06:08:39 +0200

    Introduced Type Checker module

--- src/main/kotlin/type_checker/TypeChecker.kt
+++ src/main/kotlin/type_checker/TypeChecker.kt
@@ -1,4 +1,21 @@
 package type_checker
 
+import interpreter.values.Niz
+import interpreter.values.RuntimeValue
+
 class TypeChecker {
+    fun isExpectedPrimitiveType(expectedTypename: String, value: RuntimeValue): Boolean{
+        return value.typename == expectedTypename
+    }
+
+    fun isExpectedArrayType(expectedTypename: String, array: RuntimeValue): Boolean{
+        if(array !is Niz){
+            throw Exception("Type Error")
+        }
+        else{
+            return array.value.all {
+                it.typename == expectedTypename
+            }
+        }
+    }
 }
