883c482e3408a6d2e4413286e11f37c10c7f06c4
Armin <rejzovicarmin@gmail.com>
2023-04-23 00:57:40 +0200

    Added some built-in function to Numbers

--- src/main/kotlin/interpreter/values/Broj.kt
+++ src/main/kotlin/interpreter/values/Broj.kt
@@ -1,10 +1,30 @@
 package interpreter.values
 
 import isInt
+import kotlin.math.roundToInt
 
 data class Broj(
     override var value: Double,
-    override val builtIns: HashMap<String, RuntimeValue> = hashMapOf(),
+    override val builtIns: HashMap<String, RuntimeValue> = hashMapOf(
+        "zaokruzi" to object : NativeFunkcija(name = "zaokruzi"){
+            override fun call(vararg args: RuntimeValue): RuntimeValue {
+                if(args.isNotEmpty()){
+                    throw Exception("Function 'zaokruzi' takes 0 arguments")
+                }
+                return Broj(
+                    value = value.roundToInt().toDouble()
+                )
+            }
+        },
+        "tekst" to object : NativeFunkcija(name = "tekst"){
+            override fun call(vararg args: RuntimeValue): RuntimeValue {
+                return Tekst(
+                    value = value.toString()
+                )
+            }
+
+        }
+    ),
     override val typename: String = "broj"
 ) : RuntimeValue {
 
